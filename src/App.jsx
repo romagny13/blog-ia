fa5d69df94bbdb174cf5d02bf060d6f368b879cf
romagny13 <romagny13@yahoo.fr>
2024-11-22 01:29:31 +0100

    fix loading content on slug change

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,7 @@ const theme = {
 function Router({ children }) {
   // Récupère le basename depuis vite.config.js
   const basename = import.meta.env.BASE_URL;
+  console.log(basename);
 
   return <BrowserRouter basename={basename}>{children}</BrowserRouter>;
 }
